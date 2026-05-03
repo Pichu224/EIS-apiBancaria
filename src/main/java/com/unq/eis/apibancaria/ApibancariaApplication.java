cfe3356d9c4f7ae0a73218b6d50b9ae75d7cd46d
Pichu224 <alaninsaurralde2244@gmail.com>
2026-05-03 19:18:13 -0300

    initializer

---
+++ src/main/java/com/unq/eis/apibancaria/ApibancariaApplication.java
@@ -0,0 +1,13 @@
+package com.unq.eis.apibancaria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApibancariaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApibancariaApplication.class, args);
+	}
+
+}
